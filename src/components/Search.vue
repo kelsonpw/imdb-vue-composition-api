01bf172d156ffbf0fa45441e7bc94346e6bdf255
Kelson Warner <kelson.warner@fundingcircle.com>
2019-11-10 19:23:09 -0800

    Working baseline

---
+++ src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="search">
+    <input
+       type="text"
+       :value="movieTitle"
+       @keyup="handleChange"
+    />
+    <input @click="handleSubmit" type="submit" value="SEARCH" />
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api';
+
+export default {
+  name: 'Search',
+  props: ['search'],
+  setup({ search }, { emit }) {
+    const movieTitle = ref(search);
+
+    const handleSubmit = evt => {
+      evt.preventDefault();
+      emit('search', movieTitle.value);
+    };
+
+    const handleChange = evt => {
+      movieTitle.value = evt.target.value;
+    };
+
+    return {
+      movieTitle,
+      handleSubmit,
+      handleChange,
+    };
+  },
+};
+</script>
